3a6e64043ae4ff4f8f0ce26084c9029b7e51c7e9
Djimon Nowak <dj.nowak@gmx.de>
2021-09-17 09:00:34 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const firstPlatform = MethodChannel('com.example.platform1');
+
+  @override
+  void initState() {
+    super.initState();
+    firstPlatform.setMethodCallHandler(
+        (call) async => print('native call to first screen'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('first screen')),
+        body: Center(
+            child:
+                TextButton(child: const Text('click me'), onPressed: () {})));
+  }
+}
---
+++ lib/second_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  static const secondPlatform = MethodChannel('com.example.platform2');
+
+  @override
+  void initState() {
+    super.initState();
+    secondPlatform.setMethodCallHandler(
+        (call) async => print('Native call to second screen'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text('second screen')));
+  }
+}
